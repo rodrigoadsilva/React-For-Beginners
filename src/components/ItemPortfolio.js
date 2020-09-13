119b3b50476f37adf037de9d89d01cbbc59044f2
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-09-13 16:22:06 -0300

    Get data from Firebase

--- src/components/ItemPortfolio.js
+++ src/components/ItemPortfolio.js
@@ -4,7 +4,7 @@ const ItemPortfolio = props => {
     return (
         <div className='col-sm-4'>
             <div className='thumbnail'>
-                <img src='https://via.placeholder.com/150x100' Alt='The Placeholder' width='200' height='200' />
+                <img src='https://via.placeholder.com/150x100' Alt='The Placeholder' width='150' height='100' />
                 <p><strong>{props.itemContent.title}</strong></p>
                 <p>{props.itemContent.descrition}</p>
             </div>
