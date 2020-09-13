9379778e042a4d7056945872276240164f372c18
Rodrigo da Silva <rodrigo@rodrigoads.com>
2020-09-13 01:16:22 -0300

    Implemented routes

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# React_Para_Iniciantes_Udemy
-Arquivos do cursos de React para iniciantes ministrado pelo professor Reginaldo na plataforma Udemy - https://www.udemy.com/course/xumes-react-criando-site-para-empresa
+# Udemy course - Do Zero ao React para iniciantes
+
+React for beginners course files taught by Professor Reginaldo on the Udemy platform.
+See more info on [Do Zero ao React para iniciantes - Udemy](https://www.udemy.com/course/xumes-react-criando-site-para-empresa).
+
+### Install
+
+The application has developed using [Node.js](https://nodejs.org/) v12+ and [Yarn](https://yarnpkg.com/) v1.2+, so make sure that have this versions installed.
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ npm install
+$ yarn start
+```
+
+
--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
 
-import Site from './Site'
+import Header from './components/Header'
+import Home from './components/Home'
+import Services from './components/Services'
+import Portfolio from './components/Portfolio'
+import Prices from './components/Prices'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <Site/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+
+        <Route path='/' exact component={Home}/>
+        <Route path='/services' component={Services}/>
+        <Route path='/portfolio' component={Portfolio}/>
+        <Route path='/prices' component={Prices}/>
+        <Route path='/contact' component={Contact}/>
+
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
